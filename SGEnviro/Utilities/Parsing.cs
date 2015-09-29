61beeb2ed1bc0c2baf19d2e641ee8dba994930b6
Jerome Cheng <jerome@ayulin.net>
2015-09-29 11:40:26 +0800

    Helper method for parsing float and throwing exception with message on failure.

---
+++ SGEnviro/Utilities/Parsing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SGEnviro.Utilities
+{
+    public class Parsing
+    {
+        public static void ParseFloatOrThrowException(string value, out float destination, string message)
+        {
+            if (!float.TryParse(value, out destination))
+            {
+                throw new Exception(message);
+            }
+        }
+    }
+}
