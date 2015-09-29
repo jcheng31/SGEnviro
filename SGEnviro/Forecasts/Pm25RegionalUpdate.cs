bfd614de5f19566fd83b4f4f08c492eb49c5160f
Jerome Cheng <jerome@ayulin.net>
2015-09-29 10:43:16 +0800

    PM2.5 models.

---
+++ SGEnviro/Forecasts/Pm25RegionalUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SGEnviro.Forecasts
+{
+    public enum Region
+    {
+        Central,
+        East,
+        North,
+        South,
+        West
+    }
+
+    public class Pm25RegionalUpdate
+    {
+        public Region Region { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+
+        public DateTime Timestamp { get; set; }
+        public float Reading { get; set; }
+    }
+}
---
+++ SGEnviro/Forecasts/Pm25Update.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SGEnviro.Forecasts
+{
+    public class Pm25Update
+    {
+        public Pm25RegionalUpdate Central { get; set; }
+        public Pm25RegionalUpdate East { get; set; }
+        public Pm25RegionalUpdate North { get; set; }
+        public Pm25RegionalUpdate South { get; set; }
+        public Pm25RegionalUpdate West { get; set; }
+    }
+}
