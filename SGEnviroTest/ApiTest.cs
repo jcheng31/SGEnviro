1a9b3ee94c1c1852866e77b04ec40b0a0d56a0c8
Jerome Cheng <jerome@ayulin.net>
2015-11-14 17:01:47 +0800

    Add basic API test.

---
+++ SGEnviroTest/ApiTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SGEnviro;
+
+namespace SGEnviroTest
+{
+    [TestClass]
+    public class ApiTest
+    {
+        private string apiKey;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            apiKey = ConfigurationManager.AppSettings["ApiKey"];
+        }
+
+        [TestMethod]
+        public void TestApiCanRetrieve()
+        {
+            var api = new SGEnviroApi(apiKey);
+            var result = api.GetPsiDataAsync().Result;
+            Assert.IsNotNull(result);
+        }
+    }
+}
